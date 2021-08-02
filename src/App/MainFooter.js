27e00643c5eb29c13a031227ebbb3674641c3428
Leandro Barone <web@leandrobarone.com.ar>
2021-08-02 05:47:17 -0300

    Fix padding footer

--- src/App/MainFooter.js
+++ src/App/MainFooter.js
@@ -2,7 +2,7 @@ import './MainFooter.css'
 
 export default function MainFooter() {
 	return (
-		<footer className="mainfooter pt-5 text-smaller text-center">
+		<footer className="mainfooter py-5 text-smaller text-center">
 			React exercise for DMI, created by <a href="mailto:leandro.horacio.barone@gmail.com">Leandro Barone</a>.
 			Source available on <a href="https://github.com/LeandroBarone/dmi-test" target="_blank" rel="noreferrer">GitHub</a>.
 		</footer>
