2ba987c81334457a173461408204cd3d78b99fed
Leandro Barone <web@leandrobarone.com.ar>
2021-08-02 06:06:09 -0300

    Individual tests for showDetails

--- src/PageDetails/ShowDetails.test.js
+++ src/PageDetails/ShowDetails.test.js
@@ -3,74 +3,80 @@ import { MemoryRouter } from 'react-router-dom';
 import StateContextProvider from '../StateContext/StateContext';
 import ShowDetails from './ShowDetails';
 
-it('renders name, summary, premiered date, rating, type, language, genres and status', () => {
-  const show = {
-    name: 'Some name',
-    summary: 'Some summary',
-    premiered: 'Some premiered date',
-    type: 'Some type',
-    language: 'Some language',
-    genres: ['Genre A', 'Genre B'],
-    status: 'Some status',
-    rating: {
-      average: 9.9
-    }
+const show = {
+  name: 'Some name',
+  summary: 'Some summary',
+  premiered: 'Some premiered date',
+  type: 'Some type',
+  language: 'Some language',
+  status: 'Some status',
+  genres: ['Genre A', 'Genre B'],
+  rating: {
+    average: 9.9
+  },
+  image: {
+    original: 'Fake url'
+  },
+  _embedded: {
+    images: [{
+      resolutions: {
+        original: {
+          url: 'Fake url'
+        }
+      }
+    }]
   }
-  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
-
-  const name = screen.getByText('Some name');
-  expect(name).toBeInTheDocument();
-
-  const summary = screen.getByText('Some summary');
-  expect(summary).toBeInTheDocument();
-
-  const premiered = screen.getByText('Some premiered date');
-  expect(premiered).toBeInTheDocument();
-
-  const type = screen.getByText('Some type');
-  expect(type).toBeInTheDocument();
-
-  const language = screen.getByText('Some language');
-  expect(language).toBeInTheDocument();
-
-  const genres = screen.getByText('Genre A, Genre B');
-  expect(genres).toBeInTheDocument();
-
-  const status = screen.getByText('Some status');
-  expect(status).toBeInTheDocument();
-
-  const rating = screen.getByText('9.9');
-  expect(rating).toBeInTheDocument();
-});
+}
 
 it('renders poster', () => {
-  const show = {
-    name: 'Some name',
-    image: {
-      original: 'Fake url'
-    }
-  }
   render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
   const el = screen.getByAltText('Show poster');
   expect(el).toBeInTheDocument();
 });
 
+it('renders name', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const name = screen.getByText('Some name');
+  expect(name).toBeInTheDocument();
+});
+
+it('renders summary', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const summary = screen.getByText('Some summary');
+  expect(summary).toBeInTheDocument();
+});
+
+it('renders premiered date', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const premiered = screen.getByText('Some premiered date');
+  expect(premiered).toBeInTheDocument();
+});
+
+it('renders type', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const type = screen.getByText('Some type');
+  expect(type).toBeInTheDocument();
+});
+
+it('renders language', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const language = screen.getByText('Some language');
+  expect(language).toBeInTheDocument();
+});
+
+it('renders genres', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const genres = screen.getByText('Genre A, Genre B');
+  expect(genres).toBeInTheDocument();
+});
+
+it('renders status', () => {
+  render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
+  const status = screen.getByText('Some status');
+  expect(status).toBeInTheDocument();
+});
+
 it('renders picture gallery', () => {
-  const show = {
-    name: 'Some name',
-    image: {
-      original: 'Fake url'
-    },
-    _embedded: {
-      images: [{
-        resolutions: {
-          original: {
-            url: 'Fake url'
-          }
-        }
-      }]
-    }
-  }
   render(<StateContextProvider><MemoryRouter><ShowDetails show = { show } /></MemoryRouter></StateContextProvider>);
   const el = screen.getByAltText('Show picture');
   expect(el).toBeInTheDocument();
